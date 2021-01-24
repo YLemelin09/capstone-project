8599a526e1527d912bf84c5e61e58a3a322a9fcf
yannick <yannick.lemelin@quebecormedia.com>
2021-01-24 15:44:32 -0500

    YL_SPCN implement transfer token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,10 +3,15 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
     using SafeMath for uint;
+
+    //variables
     string public name = "Revok token";
     string public symbol = "RVK";
     uint256 public decimals = 18;
     uint256 public totalSupply;
+
+    //events
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     mapping(address => uint256) public balanceOf;
 
@@ -16,8 +21,11 @@ contract Token {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[msg.sender]>= _value);
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 } 
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,64 +1,95 @@
 /* eslint-disable no-undef */
-import { tokens } from './helpers'
-const Token = artifacts.require("./Token")
+import { tokens, EVM_REVERT, EVM_INVALID_ADDRESS } from "./helpers";
+const Token = artifacts.require("./Token");
 
-require("chai")
-    .use(require("chai-as-promised"))
-    .should()
+require("chai").use(require("chai-as-promised")).should();
 
 contract("Token", ([deployer, receiver]) => {
-    const name = 'Revok token'
-    const symbol = 'RVK'
-    const decimals = "18"
-    const supply = tokens(1000000).toString()
-    let token
-  
-    
-    beforeEach(async () => {
-      token = await Token.new()
-    })
+  const name = "Revok token";
+  const symbol = "RVK";
+  const decimals = "18";
+  const supply = tokens(1000000).toString();
+  let token;
 
-    describe("deployment", () => {
-        it("tracks the name", async () => {
-           const result = await token.name()
-           result.should.equal(name)
-        })
+  beforeEach(async () => {
+    token = await Token.new();
+  });
 
-        it("tracks the symbol", async () => {
-            const result = await token.symbol()
-            result.should.equal(symbol) 
-         })
+  describe("deployment", () => {
+    it("tracks the name", async () => {
+      const result = await token.name();
+      result.should.equal(name);
+    });
 
-         it("tracks the decimals", async () => {
-            const result = await token.decimals()
-            result.toString().should.equal(decimals)
-         })
+    it("tracks the symbol", async () => {
+      const result = await token.symbol();
+      result.should.equal(symbol);
+    });
 
-         it("tracks the total supply", async () => {
-            const result = await token.totalSupply()
-            result.toString().should.equal(supply.toString())
-         })
+    it("tracks the decimals", async () => {
+      const result = await token.decimals();
+      result.toString().should.equal(decimals);
+    });
 
-         it('assigns the total supply to the deployer', async ()  => {
-            const result = await token.balanceOf(deployer)
-            result.toString().should.equal(supply.toString())
-          })
+    it("tracks the total supply", async () => {
+      const result = await token.totalSupply();
+      result.toString().should.equal(supply.toString());
+    });
 
-    })
+    it("assigns the total supply to the deployer", async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(supply.toString());
+    });
+  });
 
-    describe('Sending tokens', () => {
-       it('transfers token balance', async () => {
-         let balanceOf
-         balanceOf =  await token.balanceOf(deployer)
-         console.log('deployer balance before transfer', balanceOf.toString())
-         balanceOf = await token.balanceOf(receiver);
-         console.log('receiver balance before transfer', balanceOf.toString())
+  describe("Sending tokens", () => {
+    let result;
+    let amount;
 
-         await token.transfer(receiver, tokens(100), {from: deployer})
-         balanceOf =  await token.balanceOf(deployer)
-         console.log('deployer balance after transfer', balanceOf.toString())
-         balanceOf = await token.balanceOf(receiver);
-         console.log('receiver balance after transfer', balanceOf.toString())
-       })
-    })
-})
+    describe("success", async () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        result = await token.transfer(receiver, amount, { from: deployer });
+      });
+
+      it("transfers token balance", async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
+      });
+
+      it("emits a transfer event", async () => {
+        const log = result.logs[0];
+        log.event.should.equal("Transfer");
+        const event = log.args;
+        event.from.toString().should.equal(deployer, "from is correct");
+        event.to.toString().should.equal(receiver, "to is correct");
+        event.value
+          .toString()
+          .should.equal(amount.toString(), "value is correct");
+      });
+    });
+
+    describe("failure", async () => {
+      it("rejects insufficients balances", async () => {
+        let invalidAmount;
+        invalidAmount = tokens(1000000000); //100 million - greater than supply
+        await token
+          .transfer(receiver, invalidAmount, { from: deployer })
+          .should.be.rejectedWith(EVM_REVERT);
+
+        invalidAmount = tokens(10); //no tokens
+        await token
+          .transfer(deployer, invalidAmount, { from: receiver })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it("rejects invalid recipients", async () => {
+        await token.transfer(0x0, amount, { from: deployer }).should.be
+          .rejected;
+      });
+    });
+  });
+});
